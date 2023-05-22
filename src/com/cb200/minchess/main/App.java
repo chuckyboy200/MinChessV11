c2bdca8f2340ba8d46e3fcb6866c05b0e487aedf
chuckyboy200 <60534010+chuckyboy200@users.noreply.github.com>
2023-05-23 11:00:42 +1200

    Test

--- src/com/cb200/minchess/main/App.java
+++ src/com/cb200/minchess/main/App.java
@@ -4,12 +4,14 @@ import javax.swing.SwingUtilities;
 
 import com.cb200.minchess.board.Board;
 import com.cb200.minchess.gui.GameWindow;
+import com.cb200.minchess.perft.Perft;
 
 public class App {
     public static void main(String[] args) throws Exception {
         SwingUtilities.invokeLater(() -> {
             GameWindow gameWindow = new GameWindow();
             gameWindow.update(Board.startingPosition());
+            Perft.all();
         });
     }
 }
